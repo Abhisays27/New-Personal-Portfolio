d13dd43c80aca3fa779a299349e120207279abf5
abhishek <abhishek.nt94@gmail.com>
2024-01-18 02:31:42 -0800

    Fixed resume section

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
+import { Viewer } from '@react-pdf-viewer/core';
+import { Worker } from '@react-pdf-viewer/core';
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import packageJson from '../../../package.json';
 import resumePDF from "../../Assets/AbhishekN_Resume.pdf";
-import "../Resume/resume.css"
-
+import "../Resume/resume.css";
 import "../Home/home.css";
 
 function Resume() {
+  const [pageNumber, setPageNumber] = useState(1);
+  const pdfjsVersion = packageJson.dependencies['pdfjs-dist'];
+
   return (
     <Container fluid className="resume-section">
       <Container>
-        <h1 className="resume-heading font1">My <span className="primary-header">Resume</span></h1>
+        <h1 className="resume-heading font1">
+          My <span className="primary-header">Resume</span>
+        </h1>
         <p className="resume-description font1">
           Here you can view and download my resume.
         </p>
+
+        {/* Show PDF viewer on all screens */}
         <div className="resume-viewer">
-          <object
-            data={resumePDF}
-            type="application/pdf"
-            width="90%"
-            height="1000px"
-          >
-            <p>
-              Your browser does not support embedded PDFs. Please{" "}
-              <a href={resumePDF} target="_blank" rel="noopener noreferrer">
-                download the PDF
-              </a>{" "}
-              to view it.
-            </p>
-          </object>
+        
+          
+<Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}>
+<div className="resumecss" >
+<Viewer theme='dark' fileUrl={resumePDF}  />
+         
         </div>
-        <div className="resume-download">
+
+ 
+</Worker>
+         
+           
+       
+        </div>
+
+        {/* Show download link on all screens */}
+        <div className="resume-download text-center">
           <a href={resumePDF} target="_blank" rel="noopener noreferrer" download>
             Download Resume
           </a>
